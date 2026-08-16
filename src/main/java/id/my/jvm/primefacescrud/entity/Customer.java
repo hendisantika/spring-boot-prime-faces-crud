f137fbd9cee65bfef89ed20bb708a9c88dfd15f5
hendisantika <hendisantika@yahoo.co.id>
2026-08-16 10:50:10 +0700

    feat: add Customer JPA entity ✨🗃️

---
+++ src/main/java/id/my/jvm/primefacescrud/entity/Customer.java
@@ -0,0 +1,42 @@
+package id.my.jvm.primefacescrud.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-prime-faces-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/08/26
+ * Time: 10.48
+ */
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String phone;
+
+    private String address;
+}
